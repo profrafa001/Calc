d4fd8d7a9dc53c2497f3cea636c563d4a888f9fc
Rafaela Pessin <profrafa001@gmail.com>
2026-02-03 10:33:54 -0300

    criando uma calculadora

---
+++ calculadora.py
@@ -0,0 +1,23 @@
+print("Calculadora usando Match Case")
+print("Soma (+)\nSubtração (-)\nMultiplicação (*)\nDivisão(/)\n")
+
+num1 = int(input("Digite o primeiro número: "))
+num2 = int(input("Digite o segundo número: "))
+operador = input("Digite o operador: ")
+
+match operador:
+    case "+":
+        resultado = num1 + num2
+    case "-":
+        resultado = num1 - num2
+    case "*":
+        resultado = num1 * num2
+    case "/":
+        if num2 == 0:
+            resultado = "Erro de divisão por zero!"
+        else:
+            resultado = num1 / num2
+    case _:
+       resultado = "Operador inválido!"
+
+print("Resultado: ", resultado)
